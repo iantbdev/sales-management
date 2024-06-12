adbdc8007d2b3d9dcc2d0b50beb4a4fce95eb0d9
ian <108193413+iantbdev@users.noreply.github.com>
2024-06-12 18:10:46 -0300

    log-out do usuário funcional

--- src/Components/LandingPage/landingPage.jsx
+++ src/Components/LandingPage/landingPage.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import styles from "./landingPage.module.css";
 import { useNavigate } from "react-router-dom";
-
-// import { ReactComponent as Banner } from "/src/img/Sample.svg";
+import { logout } from "../../store/reducers/userReducer";
+import { useDispatch, useSelector } from "react-redux";
+import Banner from "../../img/Sample.svg?react";
 
 const landingPage = () => {
-  //   window.addEventListener("scroll", console.log("scroll usado"));
-  //   const [scrollPosition, setScrollPosition] = useState(0);
-
-  //   const handleScroll = (e) => {
-  //     const { scrollTop, scrollHeight, clientHeight } = e.target;
-  //     const position = Math.ceil(
-  //       (scrollTop / (scrollHeight - clientHeight)) * 100
-  //     );
-  //     setScrollPosition(position);
-  //   };
-
+  const user = useSelector((state) => state.users.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  // Log-out do usuário
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
 
   return (
     <section className={styles.container}>
       <div>
-        <h1>Bem vindo ao Gerenciador de Vendas, usuário!</h1>
+        <h1>Bem vindo ao Gerenciador de Vendas, {user.name}!</h1>
         <h2>Consulte, adicione, edite e exclua suas vendas.</h2>
         <h1></h1>
-        {/* <button>Logout</button> */}
         <button onClick={() => navigate("/management")}>Gerenciar</button>
         <span>ou</span>
-        <button>Logout</button>
+        <button onClick={() => handleLogout()}>Logout</button>
       </div>
       <div className={styles.banner}>
-        <img src="./src/img/Sample.svg" alt="Banner do Site" />
-        {/* <Banner /> */}
+        <Banner className={styles.svg} />
       </div>
     </section>
   );

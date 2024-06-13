0fc9484860c18400d4d5c358927c3a2460c9b890
ian <108193413+iantbdev@users.noreply.github.com>
2024-06-13 16:24:21 -0300

    update

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Header = () => {
   return (
-    <div class="curva">
+    <div className="curva">
       <svg
         data-name="Layer 1"
         xmlns="http://www.w3.org/2000/svg"
@@ -11,7 +11,7 @@ const Header = () => {
       >
         <path
           d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-          class="shape-fill"
+          className="shape-fill"
         ></path>
       </svg>
     </div>
